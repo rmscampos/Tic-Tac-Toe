4935dfcce5697df318f05524b21c2a9321551953
Regina Campos <rmscampos@gmail.com>
2019-10-25 15:04:34 -0500

    created game board/css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log('HOLA');
+/*----- constants -----*/
+//value of x (1)
+//value of o (-1)
+//color of values (x=purple, o=green)
+//8 possible winning combos
+const X = 1
+const O = -1
+
+
+
+
+
+
+
+/*----- app's state (variables) -----*/
+//board array 
+//whose turn it is
+//winner
+
+
+
+
+/*----- cached element references -----*/
+
+
+
+
+/*----- event listeners -----*/
+//when a cell is clicked
+//when play again button is clicked
+//
+
+
+
+
+/*----- functions -----*/
+//event handlers
+//
