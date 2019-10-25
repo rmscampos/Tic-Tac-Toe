3282134cae51a58005f7c3b679d564584d25870a
Regina Campos <rmscampos@gmail.com>
2019-10-25 17:01:24 -0500

    add event listener

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 /*----- constants -----*/
 //value of x (1)
 //value of o (-1)
-//color of values (x=purple, o=green)
 //8 possible winning combos
-const X = 1
-const O = -1
-
-
-
-
-
-
+const player1 = 'X';
+const player2 = 'O';
+const winning_combo = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 /*----- app's state (variables) -----*/
-//board array 
 //whose turn it is
 //winner
 
-
+// let 
 
 
 /*----- cached element references -----*/
@@ -28,11 +30,20 @@ const O = -1
 /*----- event listeners -----*/
 //when a cell is clicked
 //when play again button is clicked
-//
 
+document.querySelector('.board').addEventListener('click', handleCellClick);
+ 
+
+// document.getElementById('play-again')
+//     .addEventListener('click', handleButtonClick);
 
 
 
 /*----- functions -----*/
-//event handlers
-//
+//handlers(deals with event)
+//global state
+
+function handleCellClick(evt) {
+    
+    console.log(evt);
+}
